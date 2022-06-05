b68a93376f393d4b94f865be0c11a1de80065dfb
kastastin <kastastin.main@gmail.com>
2022-06-05 20:35:26 +0300

    video-24 [for & if statements]

--- js-step-1/script.js
+++ js-step-1/script.js
@@ -1,40 +1,49 @@
 "use strict";
 
 const numberOfFilms = +prompt("How many films have you already watched?", "5");
-const firstFilm = prompt("Write one of the last movies you watched", ""),
-      firstFilmRate = prompt("How much would you rate it?", ""),
-      secondFilm = prompt("Write one of the last movies you watched", ""),
-      secondFilmRate = prompt("How much would you rate it?", "");
 
 const personalMovieDB = {
-    count: 0,
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-personalMovieDB.count = numberOfFilms;
-personalMovieDB.movies[firstFilm] = firstFilmRate;
-personalMovieDB.movies[secondFilm] = secondFilmRate;
+for (let i = 0; i < 2; i++) {
+    const filmName = prompt("Write one of the last movies you watched", ""),
+          filmRate = prompt("How much would you rate it?", "");
+    
+    if (filmName != null && filmRate != null && 
+        filmName != "" && filmRate != "" && filmName.length < 50) {
+        personalMovieDB.movies[filmName] = filmRate;
+        console.log("Done!");
+    } else {
+        console.log("Error!");
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Watched quite a few films");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("You are a classic spectator");
+} else if (personalMovieDB.count >= 30) {
+    console.log("You are a cinephile");
+} else {
+    console.log("Something went wrong!");
+}
+
 console.log(personalMovieDB);
 
 
 /* Задание на урок:
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-Проверить, чтобы все работало без ошибок в консоли */
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как 
+str.length - и получить её длину)
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+4) Потренироваться и переписать цикл еще двумя способами*/
