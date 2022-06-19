f91c637e10106b9c3dd14758a4ac92d2f16f7711
kastastin <kostya242421@gmail.com>
2022-06-19 23:28:06 +0300

    video-29 [functions]

--- js-step-1/script.js
+++ js-step-1/script.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const numberOfFilms = +prompt("How many films have you already watched?", "5");
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("How many films have you already watched?", "5");
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("How many films have you already watched?", "5");
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,40 +20,59 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const filmName = prompt("Write one of the last movies you watched", ""),
-          filmRate = prompt("How much would you rate it?", "");
-    
-    if (filmName != null && filmRate != null && 
-        filmName != "" && filmRate != "" && filmName.length < 50) {
-        personalMovieDB.movies[filmName] = filmRate;
-        console.log("Done!");
-    } else {
-        console.log("Error!");
-        i--;
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const filmName = prompt("Write one of the last movies you watched", ""),
+              filmRate = prompt("How much would you rate it?", "");
+        
+        if (filmName != null && filmRate != null && 
+            filmName != "" && filmRate != "" && filmName.length < 50) {
+            personalMovieDB.movies[filmName] = filmRate;
+            console.log("Done!");
+        } else {
+            console.log("Error!");
+            i--;
+        }
     }
 }
 
-if (personalMovieDB.count < 10) {
-    console.log("Watched quite a few films");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("You are a classic spectator");
-} else if (personalMovieDB.count >= 30) {
-    console.log("You are a cinephile");
-} else {
-    console.log("Something went wrong!");
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Watched quite a few films");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("You are a classic spectator");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("You are a cinephile");
+    } else {
+        console.log("Something went wrong!");
+    }
 }
 
-console.log(personalMovieDB);
+detectPersonalLevel();
 
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        personalMovieDB.genres[i] = prompt(`Your favorite genre is number ${i + 1}`);
+    }
+}
+
+writeYourGenres();
 
 /* Задание на урок:
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как 
-str.length - и получить её длину)
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-4) Потренироваться и переписать цикл еще двумя способами*/
+1) Первую часть задания повторить по уроку
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+P.S. Функции вызывать не обязательно */
