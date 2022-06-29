a1603f6918a74ca3f35799c917fe23bbdb648470
kastastin <kostya242421@gmail.com>
2022-06-29 13:48:11 +0300

    video-64 [project Food + tabs]

---
+++ Food_dist/js/script.js
@@ -0,0 +1,39 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            // item.style.display = 'none';
+            item.classList.remove('show', 'fade'); 
+            item.classList.add('hide'); 
+        });
+        tabs.forEach(tab => {
+            tab.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        // tabsContent[i].style.display = 'block';
+        tabsContent[i].classList.remove('hide');
+        tabsContent[i].classList.add('show', 'fade');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (e) => {
+        const target = e.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+});
