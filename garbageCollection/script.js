3326d8df75cb75f6c9c5083dcdf9353716cee82e
kastastin <kostya242421@gmail.com>
2022-06-30 15:39:46 +0300

    video-66 [garbage collection & memory inspector]

---
+++ garbageCollection/script.js
@@ -0,0 +1,40 @@
+// function func() {
+//     // window.smth = 'string';
+//     smth = 'string';
+// }
+
+
+// const someRes = getData();
+// const node = document.querySelector('.class');
+
+// setInterval(function() {
+//     if (node) {
+//         node.innerHTML = someRes;
+//     }
+// }, 1000);
+
+
+// function outer() {
+//     const potentiallyHugeArray = [];
+//     return function inner() {
+//         potentiallyHugeArray.push('hello');
+//         console.log();
+//     };
+// }
+
+// const sayHello = outer();
+
+
+function createElement() {
+    const div = document.createElement('div');
+    div.id = 'test';
+    document.body.append(div);
+}
+
+createElement();
+
+function deleteElement() {
+    document.body.removeChild(document.getElementById('test'));
+}
+
+deleteElement();
