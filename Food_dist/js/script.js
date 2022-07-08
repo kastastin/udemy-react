6997f3151872cd90909f15a66c2e9f3139662f52
kastastin <kostya242421@gmail.com>
2022-07-08 14:06:35 +0300

    video-87 [fetch API (promises)]

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -262,18 +262,8 @@ window.addEventListener('DOMContentLoaded', () => {
                 display: block;
                 margin: 0 auto;
             `;
-            // form.append(statusMessage);
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-
-            // when we use mix (XMLHttpRequest + FormData) 
-            // we haven't to use setRequestHeader
-            // request.setRequestHeader('Content-type', 'multipart/form-data');
-            
-            // <-to send JSON
-            request.setRequestHeader('Content-type', 'application/json');
             const formData = new FormData(form);
             
             const obj = {};
@@ -281,21 +271,22 @@ window.addEventListener('DOMContentLoaded', () => {
                 obj[key] = value;
             });
 
-            const json = JSON.stringify(obj);
-            request.send(json);
-            // ->
-
-            // request.send(formData);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
+            fetch('server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(obj)
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
             });
         });
     }
@@ -323,4 +314,14 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     }
+
+    // fetch('https://jsonplaceholder.typicode.com/posts', {
+    //     method: 'POST',
+    //     body: JSON.stringify({name: 'Alex'}),
+    //     headers: {
+    //         'Content-type': 'application/json'
+    //     }
+    // })
+    // .then(response => response.json())
+    // .then(json => console.log(json));
 });
