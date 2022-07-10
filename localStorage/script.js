421144312201de1b8b1014efac03c0069054e3cd
kastastin <kostya242421@gmail.com>
2022-07-10 15:21:34 +0300

    video-95 [localStorage]

---
+++ localStorage/script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// localStorage.setItem('number', 5);
+// localStorage.getItem('number');
+// localStorage.removeItem('number');
+// localStorage.clear();
+
+const checkbox = document.querySelector('#checkbox'),
+      form = document.querySelector('form'),
+      change = document.querySelector('#color');
+
+if (localStorage.getItem('isChecked')) {
+    checkbox.checked = true;
+}
+
+if (localStorage.getItem('bg') === 'changed') {
+    form.style.backgroundColor = 'red';
+}
+
+checkbox.addEventListener('change', () => {
+    localStorage.setItem('isChecked', true);
+});
+
+change.addEventListener('click', () => {
+    if (localStorage.getItem('bg') === 'changed') {
+        localStorage.removeItem('bg');
+        form.style.backgroundColor = 'white';
+    } else {
+        localStorage.setItem('bg', 'changed');
+        form.style.backgroundColor = 'red';
+    }
+});
+
+
+const person = {
+    name: 'Alex',
+    age: 25
+};
+
+const serializedPerson = JSON.stringify(person);
+localStorage.setItem('alex', serializedPerson);
+
+console.log(JSON.parse(localStorage.getItem('alex')));
