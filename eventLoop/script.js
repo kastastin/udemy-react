23dc602170878d5a570071faeeb225189b4befcc
kastastin <kostya242421@gmail.com>
2022-07-14 13:24:52 +0300

    video-115 [microtasks & macrotasks]

--- eventLoop/script.js
+++ eventLoop/script.js
@@ -23,9 +23,33 @@
 // count();
 
 
-setTimeout(() => {
-    console.log(1);
-}, 0); // default instead of 0 -> 4ms
+// setTimeout(() => {
+//     console.log(1);
+// }, 0); // default instead of 0 -> 4ms
 
-console.log(2);
-// output -> 2, 1
+// console.log(2);
+// // output -> 2, 1
+
+
+
+// Microtasks & Macrotasks
+
+setTimeout(() => console.log('timeout'));
+
+Promise.resolve()
+    .then(() => console.log('promise 1'));
+
+queueMicrotask(() => console.log('queueMicrotask()'));
+
+Promise.resolve()
+    .then(() => console.log('promise 2'));
+
+console.log('code');
+
+// () => {}
+// microtasks: then / catch / finally / await
+// render
+// () => {}
+// microtasks: then / catch / finally / await
+// render
+// () => {}
