a1bd2ba2156bb3bfa67da03b1902f0dfce49b26f
kastastin <kostya242421@gmail.com>
2022-07-16 18:33:15 +0300

    video-123 [elements & components]

--- react-first-app/my-app/src/App.js
+++ react-first-app/my-app/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
+
+
+const Header = () => {
+  return <h2>Header h2</h2>
+};
+
+// const Field = () => {
+//   const holder = 'Enter here';
+//   const styledField = {
+//     width: '300px'
+//   };
+//   return <input
+//           placeholder={holder}
+//           type='text'
+//           style={styledField}/>
+// };
+
+class Field extends Component {
+  render() {
+    const holder = 'Enter here';
+    const styledField = {
+      width: '300px'
+    };
+
+    return <input
+            placeholder={holder}
+            type='text'
+            style={styledField}/>
+  }
+}
+
+function Btn() {
+  const text = 'Log In'
+  // const res = () => {
+  //   return text
+  // }
+  // return <button>{res()}</button>
+  const logged = true;
+  return <button>{logged ? 'Enter' : text}</button>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Field/>
+      <Btn/>
     </div>
   );
 }
 
+export {Header};
 export default App;
--- react-first-app/my-app/src/index.js
+++ react-first-app/my-app/src/index.js
@@ -2,38 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-// const elem = <h2>Hello World!</h2> // JSX syntax (React element)
-// const elem = React.createElement('h2', {className: 'greetings'}, 'Hello World!');
-// const element = {
-//   type: 'h2',
-//   props: {
-//     className: 'greetings',
-//     children: 'Hello World!'
-//   }
-// };
-
-const text = 'Hello World!';
-
-const elem = (
-  <div>
-    {/* <h2>Hello World!</h2> */}
-    {/* <h2>{2 + 2}</h2> */}
-    {/* <h2>{{}}</h2> // // we can't use Object in {} */}
-    {/* <h2>{new Date()}</h2> // we can't use Object in {} */}
-    {/* <h2>Text: {['ab', 'cd']}</h2> // we can use {['...']} */}
-    <h2 className='class'>Hello World!</h2>
-    <h2>{text}</h2>
-    <input type="input text" />
-    {/* <label htmlFor=""></label> */}
-    {/* <button tabIndex={0}>Click</button>
-    <button tabIndex="0">Click</button> */}
-    <button>Click</button>
-  </div>
-);
+import {Header} from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
