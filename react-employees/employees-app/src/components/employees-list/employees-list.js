50aca30516d21792f492a11927b0f49899311ef7
kastastin <kostya242421@gmail.com>
2022-07-17 19:14:46 +0300

    video-128 [add props]

--- react-employees/employees-app/src/components/employees-list/employees-list.js
+++ react-employees/employees-app/src/components/employees-list/employees-list.js
@@ -3,12 +3,18 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 import './employees-list.css';
 
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => {
+
+    const elements = data.map(elem => {
+        return (
+            // <EmployeesListItem name={elem.name} salary={elem.salary} />
+            <EmployeesListItem {...elem} />
+        );
+    });
+
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            {elements}
         </ul>
     );
 };
