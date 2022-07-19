ebf5b5bb40d894a2c2c6ae1a309b1e4d16184beb
kastastin <kostya242421@gmail.com>
2022-07-19 15:35:27 +0300

    video-132 [handling-events]

--- react-first-app/my-app/src/App.js
+++ react-first-app/my-app/src/App.js
@@ -8,24 +8,55 @@ class WhoAmI extends Component {
     super(props);
     this.state = {
       years: 27,
-      text: '+++'
-    }
+      position: ''
+    };
+    // this.nextYear = this.nextYear.bind(this);
   }
 
   nextYear = () => {
-    this.setState(state => ({
+    this.setState(state => ({ // setState() always call method render()
       years: state.years + 1
     }));
   }
 
+  // nextYear() {
+  //   this.setState(state => ({
+  //     years: state.years + 1
+  //   }));
+  // }
+
+  // commitInputChanges = (e) => {
+  //   this.setState({
+  //     position: e.target.value
+  //   });
+  // }
+
+  commitInputChanges = (e, color) => {
+    console.log(color);
+    this.setState({
+      position: e.target.value
+    });
+  }
+
   render() {
-    const {name, surname, link} = this.props;
+    const {name, surname, link} = this.props,
+          {years, position} = this.state;
 
     return (
       <div>
-        <button onClick={this.nextYear}>{this.state.text}</button>
-        <h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+        <button onClick={this.nextYear}>+++</button> 
+        {/* <button onClick={() => this.nextYear()}>+++</button>  */}
+        <h1>
+            My name is {name}, surname - {surname},
+            age - {years},
+            position - {position}
+        </h1>
         <a href={link}>My profile</a>
+        <form>
+          <span>Enter position</span>
+          {/* <input type="text" onChange={this.commitInputChanges} /> */}
+          <input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')} />
+        </form>
       </div>
     );
   }
